1c216d6ec6a0d255bdb354a7bb2826e2517ccf29
chrleal <chr.leal@gmail.com>
2022-05-02 21:59:44 -0300

    Add background color Add function that show numbers on display

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const containerButton = document.querySelector('.buttons');
+const display = document.querySelector('.display')
+const buttons = containerButton.querySelectorAll('button')
+
+buttons.forEach((button) => {
+        button.addEventListener('click', populateDisplay)
+});
+
+function populateDisplay(e) {
+    if (e.target.textContent == "1") {
+        return display.textContent = '1';    
+    } else if (e.target.textContent == "2") {
+        return display.textContent = '2';    
+    } else if (e.target.textContent == "3") {
+        return display.textContent = '3';    
+    } else if (e.target.textContent == "4") {
+        return display.textContent = '4';    
+    } else if (e.target.textContent == "5") {
+        return display.textContent = '5';    
+    } else if (e.target.textContent == "6") {
+        return display.textContent = '6';    
+    } else if (e.target.textContent == "7") {
+        return display.textContent = '7';    
+    } else if (e.target.textContent == "8") {
+        return display.textContent = '8';    
+    } else if (e.target.textContent == "9") {
+        return display.textContent = '9';    
+    } else if (e.target.textContent == "0") {
+        return display.textContent = '0';
+    } else if (e.target.textContent == "C") {
+        return display.textContent = '0';
+    }
+}
+
 function add(a,b) {
     let sum = a + b;
     return sum
@@ -31,17 +65,3 @@ function operate(operator, a, b) {
         return ('error')
     }
 }
-
-let a = 2;
-let b = 2;
-console.log(operate("+", a, b))
-console.log(operate("-", a, b))
-console.log(operate("*", a, b))
-console.log(operate("/", a, b))
-console.log(operate("l", a, b))
-console.log(add(a,b));
-console.log(subtract(a,b));
-console.log(multiply(a,b));
-console.log(divide(a,b));
-
-
